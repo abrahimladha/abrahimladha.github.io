81cab77789623f17aa3fa134fb9ca9a45cbea3d0
abrahimladha <abrahimladha@protonmail.ch>
2018-04-14 22:50:01 -0400

    minecwaft

--- README.md
+++
@@ -1,1 +0,0 @@
-# abrahimladha.github.io
---
+++ config.js
@@ -0,0 +1,52 @@
+var CONFIG = {
+  "maps": {
+    "map_myworld": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1523760070,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 10,
+      "name": "VGD Farming Valley",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "myworld_isometric_t1",
+      "tileSize": 384,
+      "world": "myworld",
+      "worldName": "myworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "map_myworld"
+  ],
+  "tileSetGroups": {
+    "myworld_isometric_t1": {
+      "maxZoom": 10,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
